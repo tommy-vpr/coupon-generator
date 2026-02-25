3410466d81eb8122d9dbd6e2bf97ce945b63c297
tommy-vpr <tommy@cultivatedagency.com>
2026-02-25 09:30:27 -0800

    Update middleware to allow all static files

--- src/middleware.ts
+++ src/middleware.ts
@@ -171,5 +171,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|gif|webp|svg|ico)$).*)",
+  ],
 };
